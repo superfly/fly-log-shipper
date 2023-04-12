6b29e83729c014591abccc91478e5cd8f7630559
Joshua Sierles <joshua@fly.io>
2023-04-12 20:08:22 +0200

    fix argument ordering now arguments are correctly passed by machine exec

--- add-logger.sh
+++ add-logger.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 BASEDIR=`dirname "$0"`
 . "$BASEDIR/common.sh"
 
-export PROVIDER_TOKEN=$1
-APP=$2
-PROVIDER=$3
-export NATS_TOKEN=$4
 
+APP=$1
+PROVIDER=$3
+export NATS_TOKEN=$3
+export PROVIDER_TOKEN=$4
  template < /etc/vector/logger.toml > /etc/vector/app-loggers/source_${APP}.toml
  template < /etc/vector/sinks/$PROVIDER.toml > /etc/vector/app-loggers/sink_${PROVIDER}_${APP}.toml
